3cdb3b1f52112acdad37f7fb74c2f28643fc435c
Ellery Engstrom <eengstrom26@students.polytechnic.org>
2026-02-19 10:06:27 -0800

    added get methods to passenger

--- Passenger.java
+++ Passenger.java
@@ -1,6 +1,39 @@
 public class Passenger {
-    public int id;
-    public int start;
+    public int location;
     public int destination;
+    private boolean arrived;
+    public int id;
+    private static int idGen = 1;
     //tostring
+
+    public Passenger(){
+        location = (int)(Math.random() * 32 + 1);
+        destination = (int)(Math.random()*32 + 1);
+        id = idGen;
+        arrived = false;
+        idGen++;
+    }
+
+    public boolean getArrivedP(){
+        return arrived;
+    }
+
+    public void isArrived(){
+        arrived = true;
+    }
+
+    public int getDestinationP(){
+        return destination;
+    }
+    public int getLocationP(){
+        return location;
+    }
+
+    public int getIDP(){
+        return id;
+    }
+
+    public void moveP(int direction){
+        location += direction;
+    }
 }
